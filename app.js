a859efa01856703875b8b67afae537aab8590c70
Emanuel <emanuel.sceppaquercia@gmail.com>
2021-08-21 19:40:21 -0300

    Added click functions

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const board = document.querySelector('.board');
     let width = 10;
     let mines = 20;
+    let gameOver = false;
     let squares = [];
 
     // create Board 
@@ -19,8 +20,54 @@ document.addEventListener('DOMContentLoaded', () => {
             square.classList.add(gameBoard[i]);
             board.appendChild(square);
             squares.push(square);
+
+            // Add event, normal click
+            square.addEventListener('click', function(e){
+                click(square);
+            });
         }
+
+        // Add total numbers of mines adjacent to the tile
+        for(let i = 0; i < squares.length; i++){
+            let total = 0;
+            const leftEdge = (i % width == 0);
+            const rightEdge = (i % width == 9);
+            
+            if(squares[i].classList.contains('tile')){
+                if(i > 0 && !leftEdge && squares[i-1].classList.contains('mine')) total++;
+                if(i > 9 && !rightEdge && squares[i+1].classList.contains('mine')) total++;
+                if(i > 9 && squares[i-width].classList.contains('mine')) total++;
+                if(i < 90 && squares[i+width].classList.contains('mine')) total++;
+                if(i > 9 && !leftEdge && squares[i-width-1].classList.contains('mine')) total++;
+                if(i > 9 && !rightEdge && squares[i-width+1].classList.contains('mine')) total++;
+                if(i < 90 && !leftEdge && squares[i+width-1].classList.contains('mine')) total++;
+                if(i < 90 && !rightEdge && squares[i+width+1].classList.contains('mine')) total++;
+            }
+            squares[i].setAttribute('data',total);
+        }
+
     }
 
     createBoard();
+
+    // Click function. Set fuctions that depends on class square
+    function click(square){
+        let currentId = square.getAttribute('id'); 
+
+        if(gameOver) return;
+        if(square.classList.contains('checked') || square.classList.contains('flag')) return;
+
+        if(square.classList.contains('mine')){
+            gameOver = true;
+        } else {
+            let total = square.getAttribute('data');
+            if(total != 0){
+                square.classList.add('checked');
+                square.innerHTML = total;
+                return;
+            }
+            square.classList.add('checked');    
+        }
+        return;
+    }
 })
